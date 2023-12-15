4af2e2aac194ebeb2d0ddbf1930ee95b422c039a
liuqiang <1057862522@qq.com>
2023-12-15 15:52:38 +0800

    changed 2

--- src/test.java
+++ src/test.java
@@ -1,17 +1,4 @@
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
-import com.jacob.activeX.ActiveXComponent;
-import com.jacob.com.ComThread;
-import com.jacob.com.Dispatch;
-import com.jacob.com.Variant;
 public class test {
     public static void main(String[] args) {
     	System.out.println("git_test");
